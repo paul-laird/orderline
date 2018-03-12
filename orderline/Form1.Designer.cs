55014f3859f63a748a60b465bb991b69d7eb19c8
89382 <paul.laird@dbs.ie>
2018-03-12 15:56:03 +0000

    Add project files.

---
+++ orderline/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace orderline
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Progress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Progress
+            // 
+            this.Progress.AutoSize = true;
+            this.Progress.Location = new System.Drawing.Point(13, 13);
+            this.Progress.Name = "Progress";
+            this.Progress.Size = new System.Drawing.Size(48, 13);
+            this.Progress.TabIndex = 0;
+            this.Progress.Text = "Progress";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 309);
+            this.Controls.Add(this.Progress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Progress;
+    }
+}
+
---
+++ orderline/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace orderline
+{
+    public partial class Form1 : Form
+    {
+        static string ConnectionString = "" +
+            "Data Source=(localdb)\\MSSQLLocalDB;" +
+            "Integrated Security=true;" +
+            "Initial Catalog=orderline;";
+        SqlConnection conn = new SqlConnection(ConnectionString);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlCommand cmd1 = new SqlCommand("" +
+                "CREATE TABLE Customer(" +
+                "ID INT PRIMARY KEY," +
+                "Name VARCHAR(25)," +
+                "Address Varchar(50));",
+                conn);
+            SqlCommand cmd2 = new SqlCommand("" +
+                "CREATE TABLE Orders(" +
+                "ID INT PRIMARY KEY," +
+                "Date Date," +
+                "CustID INT," +
+                "FOREIGN KEY(CustID) REFERENCES Customer(ID));",
+                conn);
+            SqlCommand cmd3 = new SqlCommand("" +
+                "CREATE TABLE Product(" +
+                "ID INT PRIMARY KEY," +
+                "Price INT," +
+                "Name VARCHAR(25));",
+                conn);
+            SqlCommand cmd4 = new SqlCommand("" +
+                "CREATE TABLE OrderLine(" +
+                "OID INT," +
+                "PID INT," +
+                "Quantity INT," +
+                "FOREIGN KEY(OID) REFERENCES Orders(ID), " +
+                "FOREIGN KEY(PID) REFERENCES Product(ID));",
+                conn);
+            using (conn)
+            {
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    Progress.Text = "Connection Opened";
+                    cmd1.ExecuteNonQuery();
+                    cmd2.ExecuteNonQuery();
+                    cmd3.ExecuteNonQuery();
+                    cmd4.ExecuteNonQuery();
+                    Progress.Text += "\nTables Created";
+
+                }
+            }
+        }
+    }
+}
